457f77ae8085a884f773d2049332c1014f8e8029
Yang,Zhou <thomaszdxsn@icloud.com>
2022-04-06 16:00:55 +0800

    :heavy_plus_sign: add tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <h1 className="text-xl text-red-500">Hello World</h1>
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
